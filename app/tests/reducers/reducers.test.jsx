c126696795f1f664cef669504e1908ea26ad8efb
Alec Barlow <abarlow85@gmail.com>
2016-12-20 13:10:17 -0800

    add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -30,4 +30,59 @@ describe('Reducers', () => {
 			expect(res).toEqual(true);
 		});
 	});
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			var action = {
+				type: 'ADD_TODO',
+				text: 'Walk the dog'
+			};
+
+			var res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].text).toEqual(action.text);
+		});
+
+		it('should toggle completed and add/remove completedAt attr', () => {
+			var todos = [
+				{
+					id: 1,
+					text: 'Walk the dog',
+					completed: false,
+					completedAt: undefined
+				},
+				{
+					id: 2,
+					text: 'Beat the dog',
+					completed: false,
+					completedAt: undefined
+				},
+				{
+					id: 3,
+					text: 'Kill the dog',
+					completed: false,
+					completedAt: undefined
+				}
+			]
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: 1
+			}
+
+			var res = reducers.todosReducer(df(todos), df(action));
+
+			expect(res[0].completed).toEqual(true);
+			expect(res[0].completedAt).toBeA('number');
+
+			var newRes = reducers.todosReducer(df(res), df(action));
+
+			expect(newRes[0].completed).toEqual(false);
+			expect(newRes[0].completedAt).toNotExist();
+
+		});
+	});
+
+
+
 });
