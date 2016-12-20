4b5727c9fdcf9d5a57885c722e1365c54b913afb
Alec Barlow <abarlow85@gmail.com>
2016-12-20 12:39:45 -0800

    add show completed and searchText reducers

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
 			'node_modules',
 			'./app/components/',
 			'./app/api/',
-			'./app/actions/'
+			'./app/actions/',
+			'./app/reducers/'
 		],
 		alias: {
 			applicationStyles: 'app/styles/app.scss',
